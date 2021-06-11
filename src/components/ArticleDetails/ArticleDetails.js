fa6aedfda5624493798516d729612941908b98e8
Joel <wilddroppings@gmail.com>
2021-06-10 19:03:49 -0600

    Add image with caption to descriptive page

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,11 +1,17 @@
 import './ArticleDetails.css';
-import { useLocation } from 'react-router-dom'
 
 const ArticleDetails = ({ foundArticle }) => {
 
         console.log('mew', foundArticle)
-  
-    return <h2>{foundArticle.title}</h2>
+    const image = foundArticle.multimedia[0].url
+    const caption = foundArticle.multimedia[0].caption
+    return (
+        <div className='details-card'>
+            <h2>{foundArticle.title}</h2>
+            <img className='details-image' src={image} alt={caption}/>
+            <p>{caption}</p>
+        </div>
+    )
 }
 
 export default ArticleDetails;
