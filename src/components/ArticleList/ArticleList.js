6cd9dcfac9f507950dd11872d9e08c6a7e8c2ab6
Joel <wilddroppings@gmail.com>
2021-06-10 22:53:58 -0600

    Add bottom boarder to list cards

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -8,8 +8,8 @@ const ArticleList = ({ topStories }) => {
             {topStories && topStories.map((story, index) => {
                 if (!story.title.includes('%')) {
                 return (
-                    <Link to={`/${story.title.split(' ').join('_')}`} key={index}>
-                        <p>{story.title}</p>
+                    <Link to={`/${story.title.split(' ').join('_')}`} key={index} className='thumbnail-card'>
+                        <h3>{story.title}</h3>
                         <img className='thumbnail' src={story.multimedia[0].url} alt=''/>
                         <p className='published'> Published: {story.published_date.split('T')[0]}</p>
                     </Link>
