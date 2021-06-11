65e216ead991e9247289dfa533d5693a90537d89
Joel <wilddroppings@gmail.com>
2021-06-11 10:20:46 -0600

    Change name to Nominal News

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { ReactComponent as NewsPaper } from '../../assets/newspaper.svg';
 const Header = () => {
     return (
     <header>
-        <h1>News Nerd</h1>
+        <h1>Nominal News</h1>
         <NewsPaper className='news-icon'/>
     </header>
     )
